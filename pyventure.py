e44ae7e952765f892ddb15c788545e4c89a32328
Nimbleguy <Nimbleguy@users.noreply.github.com>
2016-10-20 08:19:46 -0400

    The game is no longer nigh-impossible.

--- pyventure.py
+++ pyventure.py
@@ -4,19 +4,19 @@ import sys;
 import math;
 
 world = {}; # Nest keys are "dir" and "attr"
-#attr format: 1st bit is if win, 2-5 are place type, 6-8 are adjectives, 9 is if there are enemies, 10 is 1 if already weaponed/limbed
+#attr format: 1st bit is if win, 2-5 are place type, 6-8 are adjectives, 9 is if there are enemies, 10 is 1 if room was looted
 
 adj = ["is dusty", "is dark", "smells of fish", "groans with every step you take", "seems ready to collapse at any moment", "feels as if caution should be advised", "is quite suspicious in here", "has music blaring from an unknown source"]
-places = ["a long hallway", "a doctor\'s office", "a library", "a mine", "a lab", "a classroom", "an armory", "a canteen", "a kitchen", "a generator room", "a factory", "an empty room", "a miniature stadium", "a garden", "a farm", "a forest"];
+places = ["a long hallway", "a doctor\'s office", "a library", "an armory", "a lab", "a classroom", "an armory", "a canteen", "a kitchen", "a generator room", "a factory", "an empty room", "a miniature stadium", "a garden", "a farm", "a doctor\'s office"];
 
-monster = ["Vogon", "Worm", "Gun"];
-attacks = [{25: "reads some poetry", 7: "throws a punch"}, {15: "cast a magic spell", 1: "nibbled a limb off"}, {59: "pew-pews"}];
-health = [4, 1, 1];
+monster = ["Vogon", "Worm", "Gun", "."];
+attacks = [{25: "reads some poetry", 7: "throws a punch"}, {15: "cast a magic spell", 1: "nibbled a limb off"}, {55: "shotgunned", 19: "sniped", 30: "run'n'gunned"}, {25: "／", 15: "¶", 5: "•", 61: "‽"}];
+health = [4, 1, 1, 5];
 
 limbs = 4;
 inv = {"PUNCH": 7, "RUN": 5, "PARRY": 0, "DODGE": -10};
 
-potwep = ["SWORD", "BOW", "VOGON POEM", "WAND", "TOPKEK", "CLUB", "DEATH RAY", "MAKESHIFT EL BLANCO ARRAY", "COMIC SANS", "MICROSOFT WORD", "BANE OF HERO", "TUBA"];
+potwep = ["SWORD", "BOW", "VOGON POEM", "WAND", "TOPKEK", "CLUB", "DEATH RAY", "BOOK", "PEN", "COMIC SANS", "MICROSOFT WORD", "BANE OF HERO", "TUBA", "CHAIR", "BROKEN EL BLANCO ARRAY", "TOY GUN", "RUN"];
 
 def strife(pos):
     global world;
@@ -28,7 +28,7 @@ def strife(pos):
     print("WOULD YOU LIKE TO ENGAGE IN COMBAT‽");
     if not "y" in input("YN> ").lower():
         print("The enemy seems confused, and takes out a cellualar phone.");
-        print("You seem hear something, not knowing the sound's source.");
+        print("You seem to hear something, not knowing the sound's source.");
         print("You hear something come out of the room's loudspeakers.");
         print("\"This is Prostetnic Vogon Jeltz of the Galactic Hyperspace Planning Council...\"");
         sys.exit();
@@ -47,14 +47,15 @@ def strife(pos):
                 parried = False;
                 dc = inv[wep.upper()];
                 if(random.randint(1, 20) <= (dc % 20)):
-                    if dc is 12379:
-                        print("Oh, would you look at the time...");
+                    if dc is 12359:
+                        print("Oh, would you look at the time... H I G H N O O N");
                     if(wep.upper() == "RUN"):
                         print("Coward.");
                         return;
-                    print(str(int(math.ceil(dc / 20))) + " " + monster[typ] + " limb(s) removed!");
-                    ehp -= int(math.ceil(dc / 20));
-                    if dc is 12379:
+                    if not (wep.upper() == "PARRY" or wep.upper() == "DODGE" or wep.upper() == "RUN"):
+                        print(str(int(math.ceil(dc / 20))) + " " + monster[typ] + " limb(s) removed!");
+                        ehp -= int(math.ceil(dc / 20));
+                    if dc is 12359:
                         print("GUN turns against you!");
                         del(inv["BACK2BIZNIZ"]);
                         ehp = 1;
@@ -72,8 +73,9 @@ def strife(pos):
                     dc = random.sample(list(attacks[typ].keys()), 1)[0];
                     if dodged:
                         dc -= 10;
-                        dodged = False;
                     if(random.randint(1, 20) <= (dc % 20)):
+                        if dodged:
+                            dc += 10;
                         print(monster[typ] + " " + attacks[typ][dc] + "! You lose " + str(int(math.ceil(dc / 20))) + " limb(s)!");
                         limbs -= int(math.ceil(dc / 20));
                     else:
@@ -83,12 +85,13 @@ def strife(pos):
                         else:
                             print(monster[typ] + " missed!");
                     parried = False;
+                    dodged = False;
         if(limbs > 0):
             print("You win! You gain 1 useless murder point(s)!");
-            world[pos]["attr"] |= 256;
+            world[pos]["attr"] &= ~(1 << 8);
             if typ is 2:
                 print("You got GUN!");
-                inv["BACK2BIZNIZ"] = 12379;
+                inv["BACK2BIZNIZ"] = 12359;
         else:
             print("You are dead.");
             sys.exit();
@@ -97,24 +100,28 @@ def begin():
     global adj;
     global places;
     global limbs;
+    global inv;
     win = False;
     pos = 4545;
     ldir = 0;
     while not win:
         print("\n" * 100);
         if not "attr" in world[pos]:
-            print("You seem to have encountered a dead end.");
+            print("This room is full of nothing. Your presence disgraces this room in it's entirety.");
         else:
             print("You are in " + places[(world[pos]["attr"] >> 1) & 15] + ".");
-            if (world[pos]["attr"] >> 1) & 15 is 1 and (limbs < 7):
+            if places[(world[pos]["attr"] >> 1) & 15] == "a doctor\'s office" and limbs < 4 and not (world[pos]["attr"] >> 9) & 1 is 1:
                 print("Your broken limbs were replaced.");
-                limbs = 7;
-            if (world[pos]["attr"] >> 1) & 15 is 6 and not (world[pos]["attr"] >> 9) & 1 is 1:
+                limbs = 4;
+                world[pos]["attr"] |= 512;
+            if places[(world[pos]["attr"] >> 1) & 15] == "an armory" and not (world[pos]["attr"] >> 9) & 1 is 1:
                 wep = random.randrange(0, len(potwep));
                 print("You got " + potwep[wep] + "!");
-                inv[potwep[wep]] = random.randint(1, 29);
+                inv[potwep[wep]] = random.randint(8, 59);
+                if(inv[potwep[wep]] % 20 == 0):
+                    inv[potwep[wep]] += 1;
                 world[pos]["attr"] |= 512;
-            if (world[pos]["attr"] >> 1) & 15 is 4 and not (world[pos]["attr"] >> 9) & 1 is 1:
+            if places[(world[pos]["attr"] >> 1) & 15] == "a lab" and not (world[pos]["attr"] >> 9) & 1 is 1:
                 print("You found an extra limb!");
                 limbs+=1;
                 world[pos]["attr"] |= 512;
@@ -174,7 +181,10 @@ def init():
                 if v not in world:
                     stuff += 1;
                     world[v] = {};
-                    conn = random.randint(0, 5 - int(k / 20));
+                    if(k > 20):
+                        conn = random.randint(0, 5 - int(k / 20));
+                    else:
+                        conn = random.randint(1, 5 - int(k / 20));
                     if random.randint(0, 1) is 1 and ((k % 4) + 1 > conn):
                         conn = (k % 4) + 1;
                     pgen = [0];
@@ -218,8 +228,8 @@ def init():
                             else:
                                 world[v]["dir"] |= adir;
                             attr = 0;
-                            attr |= (random.randint(0, len(places)) << 1);
-                            attr |= (random.randint(0, len(adj)) << 5);
+                            attr |= (random.randint(0, len(places) - 1) << 1);
+                            attr |= (random.randint(0, len(adj) - 1) << 5);
                             attr |= ((random.randint(0, 2) & 1) << 8);
                             if v is 4545:
                                 attr &= 4294967039;
@@ -248,3 +258,5 @@ while not "dir" in world[4545]:
     world = {};
     init();
 begin();
+
+                
